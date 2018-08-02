51b32b6ec052307549ac61dfad2e8a2e517b05f9
Caylin Hickey <caylin.hickey@uky.edu>
2018-08-02 14:53:42 -0400

    Moving testing forward

--- src/main/java/com/researchworx/cresco/plugins/gobjectIngestion/folderprocessor/ObjectFS.java
+++ src/main/java/com/researchworx/cresco/plugins/gobjectIngestion/folderprocessor/ObjectFS.java
@@ -5,6 +5,9 @@ import com.researchworx.cresco.library.utilities.CLogger;
 import com.researchworx.cresco.plugins.gobjectIngestion.Plugin;
 import com.researchworx.cresco.plugins.gobjectIngestion.objectstorage.Encapsulation;
 import com.researchworx.cresco.plugins.gobjectIngestion.objectstorage.ObjectEngine;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.utils.IOUtils;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
@@ -133,13 +136,12 @@ public class ObjectFS implements Runnable {
                     pstep = 2;
                     return;
                 }*/
-                /*try (TarArchiveInputStream fin = new TarArchiveInputStream(new FileInputStream(baggedSequenceFile))) {
+                try (TarArchiveInputStream fin = new TarArchiveInputStream(new FileInputStream(baggedSequenceFile))) {
                     TarArchiveEntry entry;
                     while ((entry = fin.getNextTarEntry()) != null) {
                         if (entry.isDirectory()) {
                             continue;
                         }
-                        logger.trace("Extracting {}", entry.getName());
                         File curfile = new File(workDir, entry.getName());
                         File parent = curfile.getParentFile();
                         if (!parent.exists()) {
@@ -147,8 +149,7 @@ public class ObjectFS implements Runnable {
                         }
                         IOUtils.copy(fin, new FileOutputStream(curfile));
                     }
-                }*/
-                Encapsulation.decompress(baggedSequenceFile, workDir);
+                }
                 String unboxed = workDirName + seqId + "/";
                 logger.trace("unBoxIt result: {}, deleting TAR file", unboxed);
                 //new File(baggedSequenceFile).delete();
